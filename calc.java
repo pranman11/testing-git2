d3a3e949933d3b34800b8c105cfba63676f8cddb
Pranav <pranman.manglani1@gmail.com>
2019-07-30 12:07:18 +0530

    faegejnmk

---
+++ calc.java
@@ -0,0 +1,14 @@
+public class calc{
+	int add(int x, int y){
+		return (x+y);
+	}
+	int mul(int x, int y){
+		return (x*y);
+	}
+	int sub(int x, int y){
+		return (x-y);
+	}
+	int div(int x, int y){
+		return (x/y);
+	}
+}
